019bf382b541166059deac4dbdb3b1443fc601f7
inomoto daiya <daiandaian2525@gmail.com>
2022-04-06 00:33:19 +0900

    first-commit

---
+++ js/common.js
@@ -0,0 +1,50 @@
+$(function () {
+  let state = false;
+  let pos;
+  $(".hamburger, .globalMenuSp ul li a").click(function () {
+    $(".hamburger").toggleClass("active");
+    if ($(this).hasClass("active")) {
+      $(".globalMenuSp").addClass("active");
+    } else {
+      $(".globalMenuSp").removeClass("active");
+    }
+  });
+});
+
+$(function () {
+  $(".slider_wrap").slick({
+    autoplay: true,
+    autoplaySpeed: 3000,
+    dots: true,
+    prevArrow:
+      '<img src="images/product-allow-left.png" class="slide-arrow prev-arrow">',
+    nextArrow:
+      '<img src="images/product-allow-right.png" class="slide-arrow next-arrow">',
+    slidesToShow: 2,
+    slidesToScroll: 2,
+    dotsClass: "slick-dots",
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 520,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  });
+});
